3c134f36ac5c48eb4fc10f45240ff22e8ad8ff93
Karam Hijazi <khijazi@Karams-MacBook-Pro.local>
2016-04-12 08:44:20 -0600

    commited changes

--- js/app.js
+++ js/app.js
@@ -1,9 +1,61 @@
 var app = angular.module('nbaRoutes', ['ui.router']);
 
+
 app.config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
 
     $httpProvider.interceptors.push('httpRequestInterceptor');
 
-    // routing configuration code
+    // routing configuration code. $stateProvider object goes below
+    $stateProvider //provides the "states/views"
+        .state("home", { //.state("stateName") declares a new state
+            url: "/",
+            templateUrl: "js/home/homeTmpl.html", //the file path
+            controller: "homeCtrl", //each state has it's own controller
+            views: { //individual views
+                "utahjazz": {
+                    templateUrl: "js/teams/teamTmpl.html",
+                    controller: "teamCtrl",
+                    resolve: { teamData: //resolve a promise
+                        function(teamService) {//inject team service
+                            return teamService.getTeamData("utahjazz");
+
+                        }
+                    }
+                },
+                "losangeleslakers": {
+                    templateUrl: "js/teams/teamTmpl.html",
+                    controller: "teamCtrl",
+                    resolve: { teamData:
+                        function(teamService) {
+                            return teamService.getTeamData("losangeleslakers");
+                        }
+                    }
+                },
+                "miamiheat": {
+                    templateUrl: "js/teams/teamTmpl.html",
+                    controller: "teamCtrl",
+                    resolve: { teamData:
+                        function(teamService) {
+                            return teamService.getTeamData("miamiheat");
+                        }
+                    }
+                }
+            }
+        })
+        .state("teams", {
+            url: '/teams/:team', //$stateParams
+            templateUrl: "js/teams/teamTmpl.html",
+            controller: "teamCtrl",
+            resolve:{ //resolve a promise
+                teamData: function(teamService, $stateParams) {
+                    //inject teamService
+                    return teamService.getTeamData($stateParams.team);
+                    //$stateParams.team is up on line 46
+            }
+        }
+        })
+
+        $urlRouterProvider.otherwise('/');
+        //default view/state
 
 });
--- js/teams/teamCtrl.js
+++ js/teams/teamCtrl.js
@@ -1,5 +1,46 @@
 var app = angular.module('nbaRoutes');
 // the resolved data from the router needs to be injected into the controller
-app.controller('teamCtrl', function ($scope, $stateParams, teamService) {
+app.controller('teamCtrl', function ($scope, $stateParams, teamService, teamData) {
+
+    $scope.teamData = teamData;
+    $scope.newGame = {}; //newGame obj starts out emtpy in order to build it up
+    $scope.showNewGameForm = false; //newGameForm is hidden by default
+    $scope.toggleNewGameForm = function() { //on click, toggles visibility of newGameForm
+        $scope.showNewGameForm = !$scope.showNewGameForm;
+    }
+    //once you click an <a ui-sref="sdfsd" tag, it changes the $stateParams
+    if ($stateParams.team === 'utahjazz') { //if at a certain state
+        $scope.homeTeam = "Utah Jazz"; //display certain info
+        $scope.logoPath = 'images/jazz-logo.png'; //display certain pictures
+    } else if ($stateParams.team === 'losangeleslakers') {
+        $scope.homeTeam = "Los Angeles Lakers";
+        $scope.logoPath = 'images/lakers-logo.png';
+    } else if ($stateParams.team === 'miamiheat') {
+        $scope.homeTeam = "Miami Heat";
+        $scope.logoPath = 'images/heat-logo.png';
+
+    }
+
+    $scope.submitGame = function() { //once invoked
+        $scope.newGame.homeTeam = $scope.homeTeam.split(' ').join('').toLowerCase();
+        //take homeTeam and convert it to its variable name
+        teamService.addNewGame($scope.newGame) //pass newGame object to service
+        //this funciton posts the newGame data to the server
+            .then(function() {//WHAT IS THIS FIRST PROMISE DOING?
+                teamService.getTeamData($scope.newGame.homeTeam)
+                //invoke this method to retreive the data from the server, using
+                //$scope parameters
+                .then(function(data){// get the data back with a promise, using
+                    //data param to catch results. see line 44 in service.
+                    $scope.teamData = data;//assign data from service to $scope
+                    $scope.newGame = {}; //reset newGame object
+                    $scope.showNewGameForm = false; //close newGameForm
+                })
+            })
+        }
+
+
+
+
 
 });
